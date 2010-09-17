7d3db18dec27ffdd4fd59489b68b41764a16a251
Tomas Vancura <tomas.vancura@blue-point.cz>
2010-09-17 10:41:03 +0000

    Uprava instalacniho navodu podle noveho rozlozeni
    git-svn-id: svn://svn.blue-point.cz/spisovka/trunk@60 f57ce333-4fc4-42a6-9b5e-014a8da81a87

--- index.php
+++ index.php
@@ -6,16 +6,18 @@ define('WWW_DIR', dirname(__FILE__) );
 define('APP_DIR', WWW_DIR . '/app');
 // absolute filesystem path to the libraries
 define('LIBS_DIR', WWW_DIR . '/libs');
-// absolute filesystem path to the client files
-define('CLIENT_DIR', WWW_DIR . '/client');
 
-// absolute or relative url path to the public files (CSS, JS, images, ...)
+// absolute or relative url path to public dir
 define('BASE_URI', '/public/');
-// absolute or relative url path to the application root
+// absolute or relative url path to app dir
 define('BASE_APP', '/');
+
 
 // client identificator
 define('KLIENT', 'default');
+// absolute filesystem path to the client files
+define('CLIENT_DIR', WWW_DIR . '/client');
+
 
 // load bootstrap file
 require APP_DIR . '/bootstrap.php';
