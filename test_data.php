203665aa2e9024bb02497133ebf2048b574aa95d
Tomas Vancura <tomas.vancura@blue-point.cz>
2011-05-10 11:49:30 +0000

    Generátor náhodných dat
    git-svn-id: svn://svn.blue-point.cz/spisovka/trunk@142 f57ce333-4fc4-42a6-9b5e-014a8da81a87

---
+++ test_data.php
@@ -0,0 +1,44 @@
+<?php
+
+// absolute filesystem path to the web root
+define('WWW_DIR', dirname(__FILE__) );
+// absolute filesystem path to the application root
+define('APP_DIR', WWW_DIR . '/app');
+// absolute filesystem path to the libraries
+define('LIBS_DIR', WWW_DIR . '/libs');
+
+// client identificator
+define('KLIENT', 'default');
+// absolute filesystem path to the client files
+define('CLIENT_DIR', WWW_DIR . '/client');
+//define('CLIENT_DIR', WWW_DIR . '/clients/client2');
+
+// setting
+
+// smazat predchozi data?
+define('TRUNCATE', true);
+
+// generovat data 1=ANO 0=NE
+define('GENEROVAT_SPISOVE_ZNAKY', 1);
+define('GENEROVAT_SPISY', 1);
+define('GENEROVAT_SUBJEKTY', 1);
+define('GENEROVAT_ORGJEDNOTKY', 1);
+define('GENEROVAT_ZAMESTNANCE', 1);
+define('GENEROVAT_DOKUMENTY', 1);
+
+// pocet zaznamu
+define('POCET_SPISOVYCH_ZNAKU', 200);
+define('POCET_SPISU', 1500);
+define('POCET_SUBJEKTU', 3000);
+define('POCET_ORGJEDNOTEK', 300);
+define('POCET_ZAMESTNANCU', 600);
+define('POCET_DOKUMENTU', 10000);
+
+define('ADMIN_LOGIN', 'admin');
+define('ADMIN_PASSWORD', 'admin');
+
+
+
+
+// load bootstrap file
+require APP_DIR . '/test_data.php';
