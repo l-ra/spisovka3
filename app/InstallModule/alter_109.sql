1b57a98114ce1045acf9cde8dc9995895bc002e7
Tomas Vancura <tomas.vancura@blue-point.cz>
2012-05-29 09:18:44 +0000

    Odstraneni jiz nepouzivanych aktualizacnich souboru
    git-svn-id: svn://svn.blue-point.cz/spisovka/branches/s3_brno@424 f57ce333-4fc4-42a6-9b5e-014a8da81a87

--- app/InstallModule/alter_109.sql
+++
@@ -1,7 +0,0 @@
--- -----------------------------------------------------------------------------
--- revision 109
---
--- Nahradte rucne {tbls3} za odpovidajici hodnotu - prefix nebo nic
--- -----------------------------------------------------------------------------;
-
-ALTER TABLE `{tbls3}epodatelna` ADD `identifikator` TEXT NULL AFTER `isds_signature`;
