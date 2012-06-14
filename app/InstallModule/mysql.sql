cbe52f5dd34c70217792a26254d7ab46d9665bca
Tomas Vancura <tomas.vancura@blue-point.cz>
2012-06-14 08:35:59 +0000

    Aktualizace instalacniho skriptu
    git-svn-id: svn://svn.blue-point.cz/spisovka/branches/s3_brno@455 f57ce333-4fc4-42a6-9b5e-014a8da81a87

--- app/InstallModule/mysql.sql
+++ app/InstallModule/mysql.sql
@@ -888,6 +888,18 @@ INSERT INTO `{tbls3}zpusob_vyrizeni` (`id`, `nazev`, `stav`, `fixed`, `note`) VA
 (5, 'storno', 1, 0, NULL),
 (6, 'jiný způsob', 1, 0, 'fdsfdsfds');
 
+CREATE TABLE `{tbls3}stat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nazev` varchar(150) NOT NULL,
+  `kod` varchar(5) NOT NULL,
+  `stav` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `{tbls3}stat` (`id`, `nazev`, `kod`, `stav`) VALUES
+(1,	'Česká republika',	'CZE',	1),
+(2,	'Slovenská republika',	'SVK',	1);
+
 
 ALTER TABLE `{tbls3}cislo_jednaci`
   ADD CONSTRAINT `fk_cislo_jednaci_orgjednotka1` FOREIGN KEY (`orgjednotka_id`) REFERENCES `{tbls3}orgjednotka` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
