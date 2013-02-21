6cb24a3feddb940075dbd76d207916b5f4248389
Pavel Laštovička <pavel.lastovicka@blue-point.cz>
2013-02-21 15:57:04 +0100

    Doplněk k předchozí opravě.

--- app/aktualizace/570_alter.sql
+++ app/aktualizace/570_alter.sql
@@ -9,3 +9,5 @@ ALTER TABLE `{tbls3}dokument_historie`
 UPDATE `{tbls3}user_rule` SET `resource_id` = 23 WHERE `id` = 10;
 UPDATE `{tbls3}user_rule` SET `resource_id` = 22 WHERE `id` = 11;
 UPDATE `{tbls3}user_rule` SET `resource_id` = 24 WHERE `id` = 12;
+
+UPDATE `{tbls3}file` SET `mime_type` = 'application/xml' WHERE `real_name` LIKE '%.xml';
