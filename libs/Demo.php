42dd712d7ea801e9c2c51da4a2b0512bd30a60f5
Pavel Laštovička <pavel.lastovicka@blue-point.cz>
2013-10-11 15:36:03 +0200

    Přesunutí třídy Demo do adresáře app/classes

--- libs/Demo.php
+++ libs/Demo.php
@@ -1,27 +1,4 @@
 <?php
 
-class Demo {
-
-    protected static $is_demo = null;
-    
-    public static function isDemo() {
-    
-        if (self::$is_demo === null) {
-        
-            self::$is_demo = false;
-            $config = Environment::getConfig('demo');
-            if ($config !== null && $config->demo == 1) 
-                self::$is_demo = true;
-        }
-        
-        return self::$is_demo;
-    }
-
-    public static function canChangePassword($user) {
-    
-        return strstr($user->username, 'demo') === false;
-    }
-}
-
 
 ?>
