d41962059c82aff9accea2fad38f746ec89038ac
Petr Slavicek <PetrS@PetrS>
2014-04-18 15:26:19 +0200

    vynechani volani vybratSpisovyZnak() z konstruktoru

--- app/forms/controls/Select2Component.php
+++ app/forms/controls/Select2Component.php
@@ -12,8 +12,7 @@ class Select2Component extends SelectBox implements IComponent
         parent::__construct($label, $items, $size);
         $this->controlPrototype->attrs['data-widget-select2'] = 1; //pouzit widget Select2
         //Select2 options resolve nefunguje u Ajax formularu
-        $this->controlPrototype->attrs['data-widget-select2-options'] = json_encode(array('width' => 'resolve'));
-        $this->controlPrototype->onchange("vybratSpisovyZnak();");
+        $this->controlPrototype->attrs['data-widget-select2-options'] = json_encode(array('width' => 'resolve'));        
     }
     
     /**
