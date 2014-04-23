50637d7c6d856c266750d7e7e2b6273dfb8f8c91
Pavel Laštovička <pavel.lastovicka@blue-point.cz>
2014-04-23 13:49:13 +0200

    Bonz.php - drobné opravy; otestováno v praxi s Drupalem

--- app/classes/Bonz.php
+++ app/classes/Bonz.php
@@ -21,17 +21,17 @@ class BonzAgent {
         $unique_part = explode('#',$unique_info);
         
         
-        $zprava = "install_id=".$unique_part[0]."\n".
-                  "zkratka=". $klient_info->zkratka."\n".
-                  "name=".$klient_info->nazev."\n".
-                  "ic=".$klient_info->ico."\n".
-                  "tel=".$klient_info->kontakt->telefon."\n".
-                  "mail=".$klient_info->kontakt->email."\n".
-                  "version=".$app_info[0] ." (".$app_info[1].")\n".
-                  "klient_ip=".$_SERVER['REMOTE_ADDR'].")\n".
-                  "server_ip=".$_SERVER['SERVER_ADDR'].")\n".
-                  "server_name=".$_SERVER['SERVER_SOFTWARE'].")\n";
-                  
+        $zprava = "id={$unique_part[0]}\n".       // je to install_id
+                  "zkratka={$klient_info->zkratka}\n".
+                  "name={$klient_info->nazev}\n".
+                  "ic={$klient_info->firma->ico}\n".
+                  "tel={$klient_info->kontakt->telefon}\n".
+                  "mail={$klient_info->kontakt->email}\n".
+                  "version={$app_info[0]}\n".
+                  "klient_ip={$_SERVER['REMOTE_ADDR']}\n".
+                  "server_ip={$_SERVER['SERVER_ADDR']}\n".
+                  "server_name={$_SERVER['SERVER_SOFTWARE']}\n";
+
         $url = self::BONZ_URL;
         $url .= "?msg=" . base64_encode($zprava);
         
