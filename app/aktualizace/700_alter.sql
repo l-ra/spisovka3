94e047ea4a5cfe3fcafc473e82f9db9d7e1fd070
Pavel Laštovička <pavel.lastovicka@blue-point.cz>
2014-07-24 22:45:27 +0200

    Aktualizace - oprava nefunkční revize 700 v případě, když neexistovaly role spisovny (po upgradu z 3.0).

--- app/aktualizace/700_alter.sql
+++ app/aktualizace/700_alter.sql
@@ -1,10 +1,10 @@
--- ----------------------;
+-- Uživatelé, kteří upgradovali z 3.0.x verze nemají vytvořeny role pro spisovnu!! ;
 
-ALTER TABLE `{tbls3}user_rule` 
-    ADD COLUMN `order` INT NOT NULL DEFAULT '0',
-    DROP COLUMN `note`;
+-- Předpoklad - MUSÍ existovat role "referent" s ID 4. To kontroluje check skript. ;
 
--- ----------------------;
+INSERT IGNORE INTO `{tbls3}user_role` (`parent_id`, `fixed_id`, `orgjednotka_id`, `code`, `name`, `note`, `fixed`, `order`, `active`, `date_created`, `date_modified`, `sekvence`, `sekvence_string`) VALUES
+(4, NULL, NULL, 'skartacni_dohled', 'pracovník spisovny', 'Má na starost spisovnu', 1, 30, 1, NOW(), NOW(), '4.7', 'referent.4#spisovna.7'),
+(4, NULL, NULL, 'skartacni_komise', 'člen skartační komise', 'člen skartační komise, která rozhoduje o skartaci nebo archivaci dokumentu.', 1, 40, 1, NOW(), NOW(), '4.8', 'referent.4#skartacni_komise.8');
 
 INSERT INTO `{tbls3}user_resource` (`code`, `name`) VALUES ('Spisovna', 'Spisovna');
 SET @RESOURCE_ID=LAST_INSERT_ID();
--- app/aktualizace/700_code.php
+++ app/aktualizace/700_code.php
@@ -3,10 +3,10 @@
 function revision_700_check()
 {
     // --------------------------------------------------------------------    
-    $result = dibi::query("SELECT id FROM [:PREFIX:user_role] WHERE [code] = 'skartacni_dohled'");
-    
-    if (count($result) == 0)
-        throw new Exception('Role "skartacni_dohled" neexistuje! Je nutné ji vytvořit.');
+    $result = dibi::query("SELECT id FROM [:PREFIX:user_role] WHERE [code] = 'referent'");
+    $pole = $result->fetch();
+    if (!$pole || $pole['id'] != 4)
+        throw new Exception('Role "referent" je chybně definována!');
 }
 
 ?>
