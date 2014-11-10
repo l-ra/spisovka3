1a25bcafef6131b5fd291b05fb35384590c1aa83
Pavel Laštovička <pavel.lastovicka@blue-point.cz>
2014-11-10 09:58:27 +0100

    Aktualizace - Skripty 720 - 750 do archivu.

--- app/aktualizace/750_alter.sql
+++
@@ -1,7 +0,0 @@
--- ;
-
-SELECT `id` INTO @VEDOUCI_RULE_ID FROM `{tbls3}user_rule` WHERE `privilege` = 'is_vedouci';
-SELECT `id` INTO @MENIT_RULE_ID FROM `{tbls3}user_rule` WHERE `privilege` = 'menit_moje_oj';
-
-INSERT IGNORE INTO `{tbls3}user_acl` (role_id, rule_id, `allowed`) 
-  SELECT role_id, @MENIT_RULE_ID, 'Y' FROM `{tbls3}user_acl` WHERE rule_id = @VEDOUCI_RULE_ID AND `allowed` = 'Y';
