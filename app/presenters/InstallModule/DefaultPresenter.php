b90d1494f246ebf5fdbc1d071b7db6c39db23cf4
Pavel Laštovička <pavel.lastovicka@blue-point.cz>
2015-08-19 15:15:10 +0200

    Oprava Task #433: Instalační kontrola nekontroluje přítomnost ZIP extension.

--- app/presenters/InstallModule/DefaultPresenter.php
+++ app/presenters/InstallModule/DefaultPresenter.php
@@ -219,6 +219,13 @@ class Install_DefaultPresenter extends BasePresenter
                 'description' => 'Chybí rozšíření Fileinfo. Detekce MIME typů souborů bude omezena, jen podle přípony souboru.',
             ),
             array(
+                'title' => 'Rozšíření ZIP',
+                'required' => TRUE,
+                'passed' => extension_loaded('zip'),
+                'message' => 'Ano',
+                'errorMessage' => 'Chybí rozšíření Fileinfo. Aplikaci není možné nainstalovat.',
+            ),
+            array(
                 'title' => 'Zápis do dočasné složky',
                 'required' => TRUE,
                 'passed' => is_writable(CLIENT_DIR . '/temp/'),
