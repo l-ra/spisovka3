1764638be1083394b7d7669da19109c593b23f8e
Pavel Laštovička <pavel.lastovicka@blue-point.cz>
2015-10-16 13:25:12 +0200

    Přidána funkce vypnutí "kompatibilního zobrazení" v Internet Exploreru.

---
+++ app/classes/IEHttpResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Spisovka;
+
+/**
+ * Fix pro Internet Explorer a jeho "kompatibilní zobrazení".
+ * Přidá speciální HTTP hlavičku, která kompatibilní zobrazení vypne.
+ *
+ * @author Pavel Laštovička
+ */
+class IEHttpResponse extends \Nette\Http\Response
+{
+	public function __construct()
+	{
+        parent::__construct();
+
+        $this->setHeader('X-UA-Compatible', 'IE=edge');
+    }
+    
+}
