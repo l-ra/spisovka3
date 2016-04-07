38ca3be4a7800a13cd6441e65ec24ccbdb915f74
Tomas Vancura <tomas.v@goodsailors.com>
2016-04-07 14:01:00 +0200

    Uprava SQL revize 1100 - ALTER TABLE NOT NULL vyvola chybu Cannot change column 'osoba_id': used in a foreign key  constraint 'fk_user_osoba'

--- app/aktualizace/1100.sql
+++ app/aktualizace/1100.sql
@@ -1,12 +1,14 @@
 ALTER TABLE [{tbls3}user]
-  ADD [osoba_id] int NULL AFTER [active],
-  ADD CONSTRAINT [fk_user_osoba] FOREIGN KEY ([osoba_id]) REFERENCES [{tbls3}osoba] ([id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD [osoba_id] int NULL AFTER [active];
 
 UPDATE [{tbls3}user] u, [{tbls3}osoba_to_user] ou SET u.[osoba_id] = ou.[osoba_id]
   WHERE u.[id] = ou.[user_id];
 
 ALTER TABLE [{tbls3}user]
   CHANGE [osoba_id] [osoba_id] int NOT NULL;
+
+ALTER TABLE [{tbls3}user]
+  ADD CONSTRAINT [fk_user_osoba] FOREIGN KEY ([osoba_id]) REFERENCES [{tbls3}osoba] ([id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 DROP TABLE [{tbls3}osoba_to_user];
