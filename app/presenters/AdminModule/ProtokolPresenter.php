c07498d331db47eeab5b23ce08a3b706fb97d4d5
Pavel Laštovička <pavel@goodsailors.com>
2016-06-21 18:33:34 +0200

    Zobrazení ISDS protokolu, změna HTML a CSS u seznamu protokolů.

--- app/presenters/AdminModule/ProtokolPresenter.php
+++ app/presenters/AdminModule/ProtokolPresenter.php
@@ -20,4 +20,14 @@ class Admin_ProtokolPresenter extends BasePresenter
         $this->template->seznam = $LogModel->seznamPristupu($limit, $offset, $user_id);
     }
 
+    public function renderIsds()
+    {
+        $data = null;
+        $filename = ISDS_Logger::getFilename();
+        if (is_file($filename))
+            $data = file_get_contents($filename);
+        if (empty($data))
+            $data = 'Protokol je prázdný.';
+        $this->template->protocol = $data;
+    }
 }
