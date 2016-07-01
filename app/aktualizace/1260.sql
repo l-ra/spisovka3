afed3c1b0080f3f2a2024e843d9b3ad909c1904e
Pavel Laštovička <pavel@goodsailors.com>
2016-07-01 21:08:57 +0200

    app/aktualizace/1260.sql - oprava mazání starého foreign key.

--- app/aktualizace/1260.sql
+++ app/aktualizace/1260.sql
@@ -1,11 +1,13 @@
 
+ALTER TABLE [:PREFIX:user_acl]
+    DROP FOREIGN KEY [user_acl_ibfk_1];
+
+ALTER TABLE [:PREFIX:user_acl]
+    CHANGE [rule_id] [privilege_id]  int(10) unsigned NOT NULL,
+    ADD CONSTRAINT [acl_role_to_privilege_privilege] FOREIGN KEY ([privilege_id]) 
+        REFERENCES [:PREFIX:user_rule] ([id]) ON DELETE CASCADE ON UPDATE NO ACTION;
+
 RENAME TABLE [:PREFIX:user_acl] TO [:PREFIX:acl_role_to_privilege];
 RENAME TABLE [:PREFIX:user_resource] TO [:PREFIX:acl_resource];
 RENAME TABLE [:PREFIX:user_role] TO [:PREFIX:acl_role];
 RENAME TABLE [:PREFIX:user_rule] TO [:PREFIX:acl_privilege];
-
-ALTER TABLE [:PREFIX:acl_role_to_privilege]
-    DROP FOREIGN KEY [acl_role_to_privilege_ibfk_1],
-    CHANGE [rule_id] [privilege_id]  int(10) unsigned NOT NULL,
-    ADD CONSTRAINT [acl_role_to_privilege_privilege] FOREIGN KEY ([privilege_id]) 
-        REFERENCES [:PREFIX:acl_privilege] ([id]) ON DELETE CASCADE ON UPDATE NO ACTION;
