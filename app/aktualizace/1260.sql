ce6c6c3f8da61da2ad64f3dd71cba2e89663b999
Pavel Laštovička <pavel@goodsailors.com>
2016-07-01 13:20:36 +0200

    app/aktualizace/1260.sql - oprava chybějících prefixů.

--- app/aktualizace/1260.sql
+++ app/aktualizace/1260.sql
@@ -1,11 +1,11 @@
 
-RENAME TABLE [user_acl] TO [acl_role_to_privilege];
-RENAME TABLE [user_resource] TO [acl_resource];
-RENAME TABLE [user_role] TO [acl_role];
-RENAME TABLE [user_rule] TO [acl_privilege];
+RENAME TABLE [:PREFIX:user_acl] TO [:PREFIX:acl_role_to_privilege];
+RENAME TABLE [:PREFIX:user_resource] TO [:PREFIX:acl_resource];
+RENAME TABLE [:PREFIX:user_role] TO [:PREFIX:acl_role];
+RENAME TABLE [:PREFIX:user_rule] TO [:PREFIX:acl_privilege];
 
-ALTER TABLE [acl_role_to_privilege]
+ALTER TABLE [:PREFIX:acl_role_to_privilege]
     DROP FOREIGN KEY [acl_role_to_privilege_ibfk_1],
     CHANGE [rule_id] [privilege_id]  int(10) unsigned NOT NULL,
-    ADD CONSTRAINT `acl_role_to_privilege_privilege` FOREIGN KEY (`privilege_id`) 
-        REFERENCES `acl_privilege` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+    ADD CONSTRAINT [acl_role_to_privilege_privilege] FOREIGN KEY ([privilege_id]) 
+        REFERENCES [:PREFIX:acl_privilege] ([id]) ON DELETE CASCADE ON UPDATE NO ACTION;
