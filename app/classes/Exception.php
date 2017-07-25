dc4c749b837b205cdccaeaf67430d3a8e52397a1
Pavel Laštovička <pavel@goodsailors.com>
2017-07-25 12:52:43 +0200

    Oprava Task #819: Nefunkční zachytávání výjimek v aplikaci (kvůli změně namespace).

---
+++ app/classes/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Spisovka;
+
+/**
+ * Třída je nutná, protože aplikace je v namespace Spisovka.
+ * Aby stačilo psát catch (Exception $e)
+ */
+class Exception extends \Exception
+{
+
+}
